67338032da1000b24432e7edbf20a84094f67489
WANGXuecong <blold344@gmail.com>
2018-07-27 11:29:14 +1000

    vue init

---
+++ src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>Trade or View your portfolio</h1>
+        <h6>You may Save & load Data</h6>
+        <h6>Click on 'End day' to begin a new Day!</h6>
+        <hr>
+        <p>your Funds: {{funds | currency}}</p>
+    </div>    
+</template>
+
+<script>
+export default {
+    computed: {
+        funds(){
+            return this.$store.getters.funds;
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
+
---
+++ src/data/stocks.js
@@ -0,0 +1,7 @@
+export default [
+    {id: 1, name: 'BMW', price: 10},
+    {id: 2, name: 'Google', price: 2000},
+    {id: 3, name: 'MBI', price: 203},
+    {id: 4, name: 'ABC', price: 140},
+    {id: 5, name: 'Apple', price: 200}
+];
